c13d4b5010bd12a84db8e34412ed21458e99c508
Gonçalo Marantes <goncalo.marantes@intuitivo.pt>
2023-11-20 20:41:29 +0000

    chore: add declaration files

--- src/dots.ts
+++ src/dots.ts
@@ -7,6 +7,7 @@ const COEFFICIENTS: Coefficients = {
   male: [-307.75076, 24.0900756, -0.1918759221, 7.391293e-4, -1.093e-6],
   female: [-57.96288, 13.6175032, -0.1126655495, 5.158568e-4, -1.0706e-6],
 }
+
 export const dots: Formula = (weight: number, total: number, gender: Gender, mass: Mass = 'kg'): number => {
   const _total = mass === 'kg' ? total : toKilograms(total)
   const _weight = mass === 'kg' ? weight : toKilograms(weight)
